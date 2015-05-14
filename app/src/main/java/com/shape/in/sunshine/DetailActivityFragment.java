edaadf4cf9132cfe33d30077166346a8f25eb405
sera_adam <sera_adam@bitbucket.org>
2015-05-14 06:43:40 +0300

    details activity added and working code

--- app/src/main/java/com/shape/in/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/shape/in/sunshine/DetailActivityFragment.java
@@ -1,12 +1,12 @@
 package com.shape.in.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 /**
@@ -22,11 +22,16 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootview = inflater.inflate(R.layout.fragment_detail, container, false);
-        Bundle extras = getActivity().getIntent().getExtras();
 
-        TextView weatherdata = (TextView) rootview.findViewById(R.id.weatherdata) ;
-        Toast.makeText(getActivity(),extras.toString(),Toast.LENGTH_SHORT).show();
-        weatherdata.setText(extras.toString());
+        Intent intent = getActivity().getIntent();
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT))
+        {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootview.findViewById(R.id.weatherdata)).setText(forecastStr);
+        }
+
+
+
         return rootview;
     }
 }
